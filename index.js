3bf7fbe298e0979ce9d37e2f73646312f4493634
Alois Odhiambo <rayalois22@gmail.com>
2021-08-11 00:33:59 +0300

    Bug fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export {
+import {
     ISO2Continents, 
     ISO2CountryNames,
     ISO2ISO3,
@@ -7,3 +7,11 @@ export {
     ISO2CapitalCities,
     CountryCodes
 } from "./CountryCodes";
+
+module.exports = ISO2Continents;
+module.exports = ISO2CountryNames;
+module.exports = ISO2ISO3;
+module.exports = ISO2Currencies;
+module.exports = ISO2DialingCodes;
+module.exports = ISO2CapitalCities;
+module.exports = CountryCodes;
